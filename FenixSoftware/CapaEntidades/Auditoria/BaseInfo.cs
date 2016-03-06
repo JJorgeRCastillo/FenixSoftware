3088a2861d4cd05064df76b28ec690da0116760e
JJorgeRCastillo <kanh_2832@hotmail.com>
2016-03-06 05:28:04 -0500

    Creando clases basicas

---
+++ FenixSoftware/CapaEntidades/Auditoria/BaseInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades.Auditoria
+{
+    public class BaseInfo
+    {
+        #region "ATRIBUTOS"
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+        #endregion
+
+        public BaseInfo() { }
+
+        public BaseInfo(DateTime FechaCreacion, DateTime FechaActualizacion)
+        {
+
+        }
+    }
+}
---
+++ FenixSoftware/CapaEntidades/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidades.Auditoria;
+
+namespace CapaEntidades
+{
+    public class Persona
+    {
+        #region ATRIBUTOS
+        public int IdPersona { get; set; }
+        public String Nombres { get; set; }
+        public String ApPaterno { get; set; }
+        public String ApMaterno { get; set; }
+        public int Edad { get; set; }
+        public String Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public Boolean Estado { get; set; }
+        #endregion
+
+        public Persona() { }
+
+        public Persona(int IdPersona, String Nombres, String ApPaterno, String ApMaterno, int Edad,
+            String Sexo, DateTime FechaNacimiento, Boolean Estado, DateTime FechaCreacion, DateTime FechaActualizacion)
+            : base(FechaCreacion, FechaActualizacion)
+        {
+            this.IdPersona = IdPersona;
+            this.Nombres = Nombres;
+            this.ApPaterno = ApPaterno;
+            this.ApMaterno = ApMaterno;
+            this.Edad = Edad;
+            this.Sexo = Sexo;
+            this.FechaNacimiento = FechaNacimiento;
+            this.Estado = Estado;
+        }
+    }
+}
